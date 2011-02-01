28246efed66ff7dc95a67cb96b87c58a2ed15e5b
Frank M. Eriksson <code@my.domain.tld>
2011-02-01 20:15:24 +0100

    More advanced buildscript + other goodies.

---
+++ tests/TestCurl.hx
@@ -0,0 +1,17 @@
+
+/**
+	Unittests for the hxCurl wrapper of libCurl to haXe
+**/
+class TestCurl {
+	// You might want to change this
+	static var xmp_url = "http://example.com/";
+	
+	
+	public static function main() {
+		new TestCurl();
+		return 0;
+	}
+	function new(){
+		
+	}
+}
