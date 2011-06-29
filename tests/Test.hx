f3650370e8d3938d153bd9532abcd516bf427693
Frank M. Eriksson <git@knarf.se>
2011-06-29 04:04:40 +0200

    Rewrote unit tests so they use a local Node.JS server

--- tests/Test.hx
+++ tests/Test.hx
@@ -3,16 +3,19 @@
 */
 import curl.Curl;
 import neko.Lib;
+import neko.Sys;
 
 class Test {
 	
 	public static function main() {
 		// Write a welcome message that explains what this is doing
+		/*
 		Lib.println( "Theese tests requires a working internet connection." );
 		Lib.println("And that the servers it tests against is up and running.");
 		Lib.println("\nIdeally you should test against a local server that " + 
 				"is under your control. \nYou could also add a row to the " + 
 				"`\033[1;32m/etc/hosts\033[0m' file if you want. " );
+		*/
 		// This is an unusual way to testcases for me, oh well.
 		var r = new haxe.unit.TestRunner();
 		
@@ -30,13 +33,25 @@ class Test {
 class TestCurl extends haxe.unit.TestCase {
 	// You might want to change this
 	static var xmp_url = "http://git.knarf.se/hxcurltestfile";
+	static var server = "http://127.0.0.1:1337/";
+
+	/**
+		Test the [Curl.simple_fetch] function against local Server.js
+	**/
+	function testSimpleFetchLocally() {
+		var text = Curl.simple_fetch( server );
+		assertTrue( text == "Hello World\n" );
+		//Lib.println( text );
+	}
+
 	/**
 		Test the [Curl.simple_fetch] function
-	**/
-	function testSimpleFetch() {
+	**//* TODO: A good idea would be to check connectivity before failing
+	function testSimpleFetchOnline() {
 		var text = Curl.simple_fetch( xmp_url );
 		assertTrue( text.length > 32 );
 		// You might want to encomment this line depending on what is fetched.
 		Lib.println( text );
 	}
+	*/
 }
